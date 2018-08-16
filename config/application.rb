00099e62cb56b08e02b740bf955379b893712582
Pankaj Sharma <sharmapankaj090@gmail.com>
2018-08-16 22:56:46 +0530

    edits

--- config/application.rb
+++ config/application.rb
@@ -12,7 +12,6 @@ module Mytracks
     config.load_defaults 5.2
     
     config.assets.initialize_on_precompile = false
-    RAILS_ENV=production bundle exec rake assets:precompile
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
